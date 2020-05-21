afac48cc22aec7a992226454be286e0ac5f22542
bobby <bobby.hewitt@w12studios.com>
2020-05-21 18:21:30 +0100

    Updates

---
+++ precache-manifest.b5d26e6b70df5c0cac3652607e8d80df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ac2eb3613b9e253ca7f08da9584284",
+    "url": "/scanner/index.html"
+  },
+  {
+    "revision": "5e66b708c6d7342ae42a",
+    "url": "/scanner/static/css/main.6e5ef602.chunk.css"
+  },
+  {
+    "revision": "5dfb38fafaed77c7c787",
+    "url": "/scanner/static/js/2.52076dae.chunk.js"
+  },
+  {
+    "revision": "5e66b708c6d7342ae42a",
+    "url": "/scanner/static/js/main.94a0517e.chunk.js"
+  },
+  {
+    "revision": "dd9ffbb3f086685b5af2",
+    "url": "/scanner/static/js/runtime-main.eea041a1.js"
+  }
+]);
